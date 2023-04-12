b715f1960b49da6466778bc8e0f34115a4559ac5
Nathan4Andrade <nathanandrade20@gmail.com>
2023-04-12 09:06:55 -0300

    roteiro 2 testes

--- R02-01/src/test/java/sorting/test/StudentSortingTest.java
+++ R02-01/src/test/java/sorting/test/StudentSortingTest.java
@@ -7,6 +7,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import sorting.AbstractSorting;
+import sorting.divideAndConquer.MergeSort;
+import sorting.divideAndConquer.QuickSort;
+import sorting.divideAndConquer.hybridMergesort.HybridMergeSort;
+import sorting.divideAndConquer.quicksort3.QuickSortMedianOfThree;
+
 
 public class StudentSortingTest {
 
@@ -38,8 +43,11 @@ public class StudentSortingTest {
 	private void getImplementation() {
 		// TODO O aluno deve instanciar sua implementação abaixo ao invés de
 		// null
-		this.implementation = null;
-		Assert.fail("Implementation not provided");
+		//this.implementation = new HybridMergeSort<Integer>();
+		//this.implementation = new MergeSort<Integer>();
+		//this.implementation = new QuickSort<Integer>();
+		this.implementation = new QuickSortMedianOfThree<Integer>();
+	
 	}
 
 	public void populaVetorTamanhoPar(Integer[] arrayPadrao) {
